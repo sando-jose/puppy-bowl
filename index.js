31c0a82a2d56eb2ab133e403ebd073a40256ebd4
Jose <jose.sndov@gmail.com>
2024-07-15 10:56:18 -0500

    Fetch succesful

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(`Test`);
+const state = {
+  players: [],
+};
+
+const baseURL = `https://fsa-puppy-bowl.herokuapp.com/api/2406-ftb-et-web-ft`;
+
+const getPuppies = async () => {
+  try {
+    const response = await fetch(baseURL + `/players`);
+    const responseJson = await response.json();
+    const playersFromAPI = responseJson.data.players;
+    state.players = playersFromAPI;
+    console.log(playersFromAPI);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getPuppies();
